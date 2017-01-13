6ebdf0eda6dcf5a3739674c8bcb8abf96214a650
akozlowski <adrian.kozlowski@outlook.com>
2017-01-13 17:01:37 +0100

    new controllers

---
+++ src/main/java/pl/quider/web/controller/StoreController.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pl.quider.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.quider.web.allegro.SiteJournalDealsStruct;
+import pl.quider.web.allegro.UserDataStruct;
+import pl.quider.web.exception.AllegroException;
+import pl.quider.web.exception.LoginException;
+import pl.quider.web.exception.NotLoggedException;
+import pl.quider.web.service.ifc.WebServiceAllegro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Rob Winch
+ * @author Doo-Hwan Kwak
+ */
+@Controller
+@RequestMapping("/store")
+public class StoreController {
+
+    public static final String USER_NAME = "userName";
+    public static final String ALLEGRO_STATUS = "allegroStatus";
+    public static final String DEALS_COUNT = "dealsCount";
+    @Autowired
+    private WebServiceAllegro serviceClient;
+
+    @GetMapping("customers")
+    public ModelAndView viewTable(ModelMap modelMap) {
+
+        return new ModelAndView("customers/table", modelMap);
+    }
+
+    @GetMapping("offers")
+    public ModelAndView viewOffers(ModelMap modelMap) {
+
+        return new ModelAndView("customers/table", modelMap);
+    }
+
+    @GetMapping("payments")
+    public ModelAndView viewPayments(ModelMap modelMap) {
+
+        return new ModelAndView("customers/table", modelMap);
+    }
+
+}

451147386eb51a18126d5a3f2dae3199f74ce59e
akozlowski <adrian.kozlowski@outlook.com>
2017-01-22 01:31:10 +0100

    Repositories

--- src/main/java/pl/quider/web/contact/repository/AddressTypeDao.java
+++ src/main/java/pl/quider/web/contact/repository/AddressTypeDao.java
@@ -1,6 +1,7 @@
 package pl.quider.web.contact.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import pl.quider.web.contact.model.AddressType;
 
 import java.io.Serializable;
@@ -8,5 +9,6 @@ import java.io.Serializable;
 /**
  * Created by Adrian on 22.01.2017.
  */
-public interface AddressTypeDao<AddressType, Integer extends Serializable> extends CrudRepository<AddressType, Integer> {
+@Repository
+public interface AddressTypeDao extends CrudRepository<AddressType, Integer> {
 }
